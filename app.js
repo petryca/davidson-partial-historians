e2278667b9c5005d1ead18679a1726ee4a5ca9d7
David Petryca <david@petryca.com>
2022-04-05 15:54:45 +0200

    class 5.4

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 hamburger.onclick = function () {
     
     this.classList.toggle('open');
-    
+    nav.classList.toggle('show');
+    headline.classList.toggle('hide');
+    listenbtn.classList.toggle('hide');
 }
